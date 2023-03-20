6da872589e9463b54b6f5bdb60b0eb69f88f7593
Dhruvdudhat310 <106237977+Dhruvdudhat310@users.noreply.github.com>
2023-03-20 14:21:43 +0530

    Add files via upload

---
+++ java programs/practical-08.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class P_8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+      
+        System.out.println("Enter a String := ");
+        String s = sc.nextLine();
+        s = s.toLowerCase();
+        System.out.println(s);
+    }
+}
+
+
+OUTPUT:
+Enter a String := 
+DHRUV
+dhruv 
