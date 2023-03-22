449d0b685562d38e01d0d43fcc133f9d8d53f270
Dhruv Dudhat <106237977+Dhruvdudhat310@users.noreply.github.com>
2023-03-22 13:56:50 +0530

    Update practical-07.java

--- practical-07.java
+++ practical-07.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class prac_7 {
+public class p_7 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a String := ");
