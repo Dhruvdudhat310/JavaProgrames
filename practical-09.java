efd667c3feeb7ed818c0431dc6d6a36cdb292dba
Dhruv Dudhat <106237977+Dhruvdudhat310@users.noreply.github.com>
2023-03-22 14:12:08 +0530

    Update practical-09.java

--- practical-09.java
+++ practical-09.java
@@ -1,35 +1,29 @@
-/*Write a program to find that given number or string is palindrome 
-or not.*/
-
 import java.util.Scanner;
-
-public class p_9 {
+public class prac_9 {
+    static void checkPalindrome(String s) {
+        String r = "";
+        for (int i = s.length() - 1; i >= 0; i--) {
+            r = r + s.charAt(i);
+        }
+        if(s.equals(r)) {
+            System.out.println(s + " is Palindrome.");
+        }
+        else {
+            System.out.println(s + " is not Palindrome.");
+        }
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number or string: ");
-        String input = sc.nextLine();
-
-        if (isPalindrome(input)) {
-            System.out.println(input + " is a palindrome.");
-        } else {
-            System.out.println(input + " is not a palindrome.");
-        }
+        System.out.println("Enter Number or string := ");
+        String s = sc.next();
+        System.out.println(s);
+        checkPalindrome(s);
     }
-public static boolean isPalindrome(String str) {
-    int left = 0;
-    int right = str.length() - 1;
-    while (left < right) {
-        if (str.charAt(left) != str.charAt(right)) {
-            return false;
-        }
-        left++;
-        right--;
-    }
-    return true;
-}
 }
 
 
 OUTPUT:
-Enter a number or string: 121
-121 is a palindrome.
+Enter Number or string := 
+15251
+15251
+15251 is Palindrome. 
