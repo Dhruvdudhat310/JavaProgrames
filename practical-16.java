6934ca37bd5ab7aff50c15643ebcf2fa9062a68c
Dhruv Dudhat <106237977+Dhruvdudhat310@users.noreply.github.com>
2023-04-03 22:43:40 +0530

    Update practical-16.java

--- practical-16.java
+++ practical-16.java
@@ -1,51 +1,50 @@
-/*Write a test program that prompts the user to enter ten numbers, 
-invoke a method to reverse the numbers, display the numbers.*/
-
 import java.util.Scanner;
 
 public class p_16 {
-    public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
-      
-        System.out.println("enter no.of value:");
-        int n=input.nextInt();
-        int[] numbers = new int[n];
-        System.out.print("Enter numbers: ");
-        for (int i = 0; i < n; i++) {
-            numbers[i] = input.nextInt();
+    public static void reverse(int[] arr) {
+        int i = 0, temp;
+        while (i <= arr.length / 2) {
+            temp = arr[i];
+            arr[i] = arr[arr.length - 1 - i];
+            arr[arr.length - 1 - i] = temp;
+            i++;
         }
-        reverse(numbers);
-
-        System.out.print("The reversed numbers are: ");
-        for (int i = 0; i < n; i++) {
-            System.out.print(numbers[i] + " ");
-}
-}
-public static void reverse(int[] array) {
-    int left = 0;
-    int right = array.length - 1;
-    while (left < right) {
-        int temp = array[left];
-        array[left] = array[right];
-        array[right] = temp;
-        left++;
-        right--;
     }
-}
+    public static void main(String[] args) {
+        int[] arr = new int[10];
+        Scanner input = new Scanner(System.in);
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Enter at Position " + (i + 1) + " : ");
+            arr[i] = input.nextInt();
+        }
+        reverse(arr);
+        System.out.println("After reversing numbers in an Array :");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Value at Position " + (i + 1) + " : " + arr[i]);
+        }
+    }
 }
 
 
 OUTPUT:
-enter no.of value:
-10
-Enter numbers: 3
-4
-6
-1
-9
-0
-7
-5
-1
-4
-The reversed numbers are: 4 1 5 7 0 9 1 6 4 3
+Enter at Position 1 : 1
+Enter at Position 2 : 2
+Enter at Position 3 : 3
+Enter at Position 4 : 4
+Enter at Position 5 : 5
+Enter at Position 6 : 6
+Enter at Position 7 : 7
+Enter at Position 8 : 8
+Enter at Position 9 : 9
+Enter at Position 10 : 10
+After reversing numbers in an Array :
+Value at Position 1 : 10
+Value at Position 2 : 9
+Value at Position 3 : 8
+Value at Position 4 : 7
+Value at Position 5 : 5
+Value at Position 6 : 6
+Value at Position 7 : 4
+Value at Position 8 : 3
+Value at Position 9 : 2
+Value at Position 10 : 1 
