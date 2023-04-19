59936a534000be2aa80427325ce900c53a5b9485
Dhruv Dudhat <106237977+Dhruvdudhat310@users.noreply.github.com>
2023-04-19 11:21:09 +0530

    Update practical-27.java

--- practical-27.java
+++ practical-27.java
@@ -1,1 +1,31 @@
+abstract class A {
+    public abstract void display();
+}
+
+class A1 extends A {
+    public void display() {
+        System.out.println("This is A1's display method.");
+    }
+}
+
+class A2 extends A {
+    public void display() {
+        System.out.println("This is A2's display method.");
+    }
+}
+
+public class prac_27 {
+    public static void main(String[] args) {
+        A1 a1_object = new A1();
+        a1_object.display();
+        A2 a2_object = new A2();
+        a2_object.display();
+    }
+}
+
+
+OUTPUT:
+This is A1's display method.
+This is A2's display method.
+
 
