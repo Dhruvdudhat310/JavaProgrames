5a94b523ecaed472297cd25a527192f3dfb58dcd
Dhruv Dudhat <106237977+Dhruvdudhat310@users.noreply.github.com>
2023-04-19 11:49:35 +0530

    Update practical-37.java

--- practical-37.java
+++ practical-37.java
@@ -1,1 +1,29 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+class p_37 {
+    public static void main(String arg[]) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Provide source file name :");
+        String sfile = sc.next();
+        System.out.print("Provide destination file name :");
+        String dfile = sc.next();
+        FileReader fin = new FileReader(sfile);
+        FileWriter fout = new FileWriter(dfile, true);
+        int c;
+        while ((c = fin.read()) != -1) {
+            fout.write(c);
+        }
+        System.out.println("Copy finish...");
+        fin.close();
+        fout.close();
+    }
+}
+
+
+OUTPUT:
+Provide source file name :First.txt
+Provide destination file name :Second.txt
+Copy finish...
+
 
